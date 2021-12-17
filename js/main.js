0d5eae511a251350dce13dd642030957f078ccb7
Ivan <ivan.kalunuch12@gmail.com>
2021-12-17 23:21:36 +0200

    New changes

--- js/main.js
+++ js/main.js
@@ -1,68 +1,39 @@
 'use strict'
 
-/*
-//масиви
-const arr = [123,332,124,1231];
 
-arr.push(171209);//добавляэ елемент у кынець
-arr.shift();//удаляє перший елемент у масиві
-arr.pop();//удаляє останній елемен у масиві
-arr.unshift('asasdb',212);//добавляє елементи на початок масива  
-arr.splice(0,1,'asd');//вирізає елементи масиву, може замінювати їх
-arr.splice(2,0, 12312,123124,124,0);
+let numberOfFilms;
 
+let start = function(){
+	while(numberOfFilms =='' || numberOfFilms === null || isNaN(numberOfFilms)|| numberOfFilms.length >= 50){
+		numberOfFilms = +prompt('Скільки ви переглянули фільмів?','');
+	};
+};
+start();
 
-const newArr = arr.slice();//копіює масив(адекватно)
-const newArr2 = arr.concat()//копіює масив(адекватно)
-const newArr3 = arr.concat('')//копіює масив(адекватно), з можливістю добавлення в кінець елемента 
+const personalMovieDB = {
+	count: numberOfFilms, 
+	movies:{
+		1: 'Harry Poter',
+		2: 'Merlin',
+		3: 'Home alone'
+	},
+	actors:{},
+	genres:{},
+	privat:true 
+};
 
-// console.log(newArr.indexOf(0));
-// console.log(newArr.lastIndexOf(`231));
-// console.log(newArr.includes(1231));
+let checkPrivatDB = function(){
+	if(personalMovieDB.privat == false){
+		console.log(personalMovieDB)
+	}
+};
+checkPrivatDB()
 
-
-console.log(newArr2);
-console.log(arr);
-console.log(newArr);
-
-
-
-const arrNumbers = [
-	{name: 'Ivan', age: 17},
-	{name: 'Iasd', age: 27},
-	{name: 'Koly', age: 12},
-];
-
-
-let nameAge = arrNumbers.find(s => s.age >= 15);//шукає істину зупиняється на першій правді
-let nameAgee= arrNumbers.filter(s => s.age >= 15);//шукає істину(усі)
-
-
-console.log(nameAge);
-console.log(nameAgee);
-
-
-
-
-let string = 'Vasya,Kolya,Vanya';
-const numbers = [1,213,123123,123531,12,323];
-const strings = ["Вася", "Коля", "Иван", "В"];
-
-
-console.log(strings.sort());
-console.log(numbers.sort((a,b) => a-b));
-console.log(strings.reverse());
-console.log(strings.map(i=>i));
-console.log(string.split(','))//розділяє строку по елементу на масив
-console.log(string.split(',', 2))//"2" ограничитель кільності елементів 
-console.log(strings.join(','))//наоборот
-
-
-for (var i = 0; i < strings.length; i++) {
-	console.log(strings[i]);
-}
-
-console.log(numbers.reduce((previousValue, item)=> previousValue + item));
-
-*/
-
+let whatYourFavoriteGenres = function(){
+	for(let i = 1; i<4; i++){
+		let whatYourFavoriteGenres = prompt(`Ваш любимий жанр під номером ${personalMovieDB.movies[i]}`);
+		personalMovieDB.genres[personalMovieDB.movies[i]] = whatYourFavoriteGenres;
+	}
+	console.log(personalMovieDB);
+};
+whatYourFavoriteGenres()
